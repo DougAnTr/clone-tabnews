2732cccf993458bfc36729563feec7d840f57d15
Douglas Trofino <douglas.trofino@outlook.com>
2024-10-10 19:33:55 +0000

    testing iframe

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
 export default function Home(){
-    return <div>Hello World!</div>
+    return <iframe src="https://lastorg.dynzup-test.com/zup/Z-336" width="540" height="500" allowFullScreen></iframe>
+
 }
