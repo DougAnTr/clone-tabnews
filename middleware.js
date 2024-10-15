491efe6b81e5060522f48bd020520bbecbd4f4a6
Douglas Trofino <douglas.trofino@outlook.com>
2024-10-15 19:04:41 +0000

    fix set of cookies

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    const response = NextResponse.redirect(new URL('/home', request.url));
+    const response = NextResponse.next();
     response.cookies.set('clone', 'tabnews');
 
   return response;
