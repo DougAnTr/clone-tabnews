4d5846689707c377fd9c840fb5cf527803217de9
Douglas Trofino <douglas.trofino@outlook.com>
2024-10-15 18:53:46 +0000

    add cookie in middleware

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
-export default function Home(){
-    return <iframe src="https://lastorg.dynzup-test.com/zup/Z-336" width="540" height="500" allowFullScreen></iframe>
+import { cookies } from 'next/headers'
 
+export default function Home(){
+    cookies().set('clone', 'tabnews');
+    
+    return <iframe src="https://lastorg.dynzup-test.com/zup/Z-336" width="540" height="500" allowFullScreen></iframe>
 }
